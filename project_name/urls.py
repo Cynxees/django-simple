7ad5dd82682af719a734e31ad4160635535cbc0a
Cynx <34434408+Cynxees@users.noreply.github.com>
2021-11-01 15:10:19 +0700

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.urls import include, path
+from . import views
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', '{{ project_name }}.views.home'),
-    
-    # Include an application:
-    # url(r'^app_name/', include('app_name.urls', namespace="app_name")),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    path('', views.Homepage.as_view(),name='homepage'),
+    path('admin/', admin.site.urls),
+]
